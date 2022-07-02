1acf2d0894e522976eae345b0ec4ac45347122ff
Himself65 <himself65@outlook.com>
2022-07-02 15:31:24 -0500

    feat: add cell types and predict function config

--- src/sheet/index.ts
+++ src/sheet/index.ts
@@ -1,5 +1,16 @@
 import type { FC } from 'react'
 
+export const CellTypes = {
+  Array: '6f84aa0b-aa88-48e3-96f8-c19f00f60ee0',
+  Boolean: '1b692f8e-6f4d-4708-bf04-e624e7101a3d',
+  Json: 'e01e830e-8dd5-4c2e-a8eb-dc3945d8c001',
+  Number: '21112007-716a-4c28-80f3-a058aea50a0b',
+  String: '00bfb075-9d08-4ab6-ba2d-f7bcccdb09b0'
+} as const
+
+type CellTypesKeys = keyof typeof CellTypes;
+export type CellType = typeof CellTypes[CellTypesKeys]
+
 export type Cell =
   (undefined | null | boolean | string | number | object)
   | Cell[]
@@ -33,7 +44,19 @@ export interface SheetTransformFunction<Config extends Record<string, unknown> =
   type: 'transform'
 }
 
-export interface SheetPredictFunction<Config extends Record<string, unknown> = Record<string, unknown>> extends SheetFunction<(
+export interface ComputeConfig {
+  inputs: {
+    type: CellType
+    name: string
+  }[]
+  outputs: {
+    type: CellType
+    name: string
+  }
+  [key: string]: unknown
+}
+
+export interface SheetPredictFunction<Config extends ComputeConfig = ComputeConfig> extends SheetFunction<(
   columns: Column[], config: Config) => Promise<Column[]>,
   Config> {
   type: 'predict'
